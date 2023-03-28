14228e1df767426fdc1add06d4ebd1daba4eff8e
Adrian Herrera <adrian.herrera@salesianos-lacuesta.net>
2023-03-28 10:43:06 +0100

    feat(Main): added user number input to sout

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        final Scanner SCANNER = new Scanner(System.in);
+        System.out.println("Introduce un número:");
+        String number = SCANNER.nextLine();
+        System.out.println("Hola Mundo " + number + "!");
+        SCANNER.close();
     }
 
 }
